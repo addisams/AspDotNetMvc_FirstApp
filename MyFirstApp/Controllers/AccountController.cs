956e1e7c900118b4f3787ac188eb2fbbd4cb551e
Adarsh Srivastav <adsrivastav307@gmail.com>
2024-02-02 19:14:06 +0530

    First App in .net mvc created.

---
+++ MyFirstApp/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string userName,string password)
+        {
+            if(userName == "admin" && password == "manager") 
+            { 
+                return RedirectToAction("Dashboard","Admin"); 
+            }
+            else
+
+            return RedirectToAction("invalidLogin");
+        }
+
+        public ActionResult invalidLogin()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyFirstApp/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            return View("OurCompanyProducts");
+        }
+
+        public ActionResult Contacts()
+        {
+            return View();
+        }
+        public ActionResult getEmpName(int empId)
+        {
+            var employees = new[]
+            {
+                new {empId=1,empName="scots", empSalery=8000 },
+                new {empId=2,empName="kevin", empSalery=2500},
+                new {empId=3,empName="raja", empSalery=3500}
+             };
+            string matchingEmpName=null;
+            foreach(var emp in employees)
+            {
+                if(emp.empId== empId)
+                {
+                    matchingEmpName = emp.empName;
+                }
+            }
+            // return new ContentResult() {Content=matchingEmpName,ContentType="text/plain" };// returns the object of content result class
+            //insted of above lengthy process we can use shorcut by calling content only
+            return Content(matchingEmpName,"text/plain");
+        }
+
+        public ActionResult getPaySlip(int empID)
+        {
+            string fileName = "~/paySlip" + empID + ".pdf";
+            return File(fileName, "application/pdf");
+        }
+        public ActionResult getEmpFacebookPage(int empID)
+        {
+            string fbUrl= "http://www.facebook.com/emp" + empID;
+            return Redirect(fbUrl);
+
+        }
+        public ActionResult studentDetails()
+        {
+            ViewBag.studentId = 101;
+            ViewBag.studentName = "Scott";
+            ViewBag.marks = 80;
+            ViewBag.noOfSem = 6;
+            ViewBag.subjects=new List<string>() {"maths","physics","chemistry" };
+            return View();
+        }
+    }
+}
